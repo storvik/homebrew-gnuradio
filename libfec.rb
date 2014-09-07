6b0be6f45080b184e46e067e92389af232e430e4
TT <edy555@gmail.com>
2014-09-07 11:32:25 +0900

    add dependency to gcc

--- libfec.rb
+++ libfec.rb
@@ -5,6 +5,8 @@ class Libfec < Formula
   url "http://www.ka9q.net/code/fec/fec-3.0.1.tar.bz2"
   sha1 "d5cac0680b055c7808e3b33876ab4e819cd318ae"
 
+  depends_on 'gcc'
+
   patch :p1 do
     url "http://lodge.glasgownet.com/bitsnbobs/kg_fec-3.0.1.patch"
     sha1 "fc25babccb5ce3e783619f886c858b28f42652b9"
