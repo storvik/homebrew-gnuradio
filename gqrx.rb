cd693c94aaba329589b9302fe2c03540de63fcc0
TT <edy555@gmail.com>
2014-09-14 07:58:58 +0900

    add url

--- gqrx.rb
+++ gqrx.rb
@@ -3,6 +3,8 @@ require 'formula'
 class Gqrx < Formula
   homepage 'https://github.com/csete/gqrx'
   head 'https://github.com/csete/gqrx.git', :branch => 'master'
+  url 'http://sourceforge.net/projects/gqrx/files/2.3.1/gqrx-2.3.1.tar.gz'
+  sha1 '0669029987329b4cd4332e20f99b099befa44c45'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
@@ -12,7 +14,7 @@ class Gqrx < Formula
   depends_on 'gr-osmosdr'
 
   def install
-    args = "PREFIX=#{prefix}" 
+    args = "PREFIX=#{prefix}"
     mkdir "build" do
       system "qmake",  *args, ".."
       system "make"
