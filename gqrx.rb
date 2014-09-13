89b4daee4021a660a44016eb48f3faacd4d0ff0a
TsCl <pmontagnier@gmail.com>
2014-09-13 18:03:45 +0200

    Removed comments

--- gqrx.rb
+++ gqrx.rb
@@ -1,9 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-# from: https://github.com/xlfe/homebrew-gnuradio
-
 class Gqrx < Formula
   homepage 'https://github.com/csete/gqrx'
   head 'https://github.com/csete/gqrx.git', :branch => 'master'
