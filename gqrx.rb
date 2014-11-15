bcff119c1c1ed45cb06c7dd607e60141cf2278e9
Andres Vahter <andres.vahter@gmail.com>
2014-11-15 17:25:14 +0200

    take gqrx from head
    it is taken from https://github.com/Stanback/homebrew-ettus-uhd/commit/d718d76814d32367a1f8759e94498a824bbc5062, however I couldn't merge it properly because this was rather larger commit

--- gqrx.rb
+++ gqrx.rb
@@ -2,9 +2,7 @@ require 'formula'
 
 class Gqrx < Formula
   homepage 'https://github.com/csete/gqrx'
-  head 'https://github.com/csete/gqrx.git', :branch => 'master'
-  url 'http://sourceforge.net/projects/gqrx/files/2.3.1/gqrx-2.3.1.tar.gz'
-  sha1 '0669029987329b4cd4332e20f99b099befa44c45'
+  head 'https://github.com/csete/gqrx.git'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
